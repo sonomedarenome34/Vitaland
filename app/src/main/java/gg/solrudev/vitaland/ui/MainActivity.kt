215d364fe9c8de890be32595d9d5c08945385800
Ilya Fomichev <fomil.music@gmail.com>
2022-05-22 06:13:10 +0700

    renaming

--- app/src/main/java/gg/solrudev/vitaland/ui/MainActivity.kt
+++ app/src/main/java/gg/solrudev/vitaland/ui/MainActivity.kt
@@ -3,7 +3,7 @@ package gg.solrudev.vitaland.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
-		val navController = findNavController(R.id.nav_host_fragment_activity_main)
+		val navController = binding.navHostFragmentActivityMain.getFragment<NavHostFragment>().navController
 		val appBarConfiguration = AppBarConfiguration(
 			setOf(
 				R.id.navigation_login, R.id.navigation_home, R.id.navigation_profile, R.id.navigation_notifications
